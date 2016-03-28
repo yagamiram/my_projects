955082bd7f6b178814683961c3fb0cce03968219
Yagamiram Elric TK <sriram.ssnit@gmail.com>
2016-03-28 17:59:41 -0500

    Update pricer.py

--- Sell_Buy_stock/pricer.py
+++ Sell_Buy_stock/pricer.py
@@ -1,8 +1,9 @@
 import heapq
 # http://rgmadvisors.com/problems/orderbook/
-
-def buy_shares_action(buy_shares, target_size, buy):
+output = open("output.txt", "w")
+def buy_shares_action(buy_shares, target_size, buy, time):
     #print "buy is ", buy
+    buy_set = set()
     if buy_shares >= target_size:
         # you can buy the shares
         shares = target_size
@@ -12,6 +13,7 @@ def buy_shares_action(buy_shares, target_size, buy):
             element = heapq.heappop(buy)
             # print "element poped is", element
             heap_list.append(element)
+            buy_set.add(element[2])
             if shares <= element[1]:
                 amount += float(element[0])  * shares
                 shares = 0
@@ -21,13 +23,16 @@ def buy_shares_action(buy_shares, target_size, buy):
                 amount += float(element[0])  * int(element[1])
             if shares == 0:
                 break
-                # print "amount is ", amount, shares
-        print "time S " + str(-amount)
+                # print "amount is ", amount, shares 
+        output.write(time +" S " + str(-amount))
+        output.write("\n")
         for each_ele in heap_list:
             heapq.heappush(buy, each_ele)
             # buy.insert(each_ele)
-def sell_shares_action(sell_shares, target_size, sell):
+    return buy_set
+def sell_shares_action(sell_shares, target_size, sell, time):
     #print "sell is", sell
+    sell_set = set()
     if sell_shares >= target_size:
         # you can buy the shares
         shares = target_size
@@ -36,6 +41,7 @@ def sell_shares_action(sell_shares, target_size, sell):
         while True:
             element = heapq.heappop(sell)
             heap_list.append(element)
+            sell_set.add(element[2])
             #print element
             if shares <= element[1]:
                 amount += float(element[0]) * shares
@@ -45,10 +51,12 @@ def sell_shares_action(sell_shares, target_size, sell):
                 amount += float(element[0]) * int(element[1])
             if shares <= 0:
                 break
-        print "time B " + str(amount)
+        output.write(time +" B " + str(amount))
+        output.write("\n")
         for each_ele in heap_list:
             heapq.heappush(sell, each_ele)
             # sell.insert(each_ele)
+    return sell_set
 def main(target_size):
     # two variables for buy and sell
     # read the csv files
@@ -59,7 +67,7 @@ def main(target_size):
     sell = []
     buy_dict = dict()
     sell_dict = dict()
-    input_file = open("sample.txt","r")
+    input_file = open("sample2.txt","r")
     #print input_file
     for each_line in input_file:
         each_values = each_line.split()
@@ -70,18 +78,18 @@ def main(target_size):
             # add it to the min heap first
             if each_values[3] == "B":
                 #print "B"
-                heapq.heappush(buy, [float(each_values[4])*-1, float(each_values[5]), each_values[2]])
+                heapq.heappush(buy, [float(each_values[4])*-1, float(each_values[5]), each_values[2], each_values[0]])
                 buy_shares += int(each_values[5])
                 buy_dict = {buy[index][2]: index for index in range(len(buy))}
-                buy_shares_action(buy_shares, target_size, buy)
+                buy_set = buy_shares_action(buy_shares, target_size, buy, each_values[0])
                 buy_dict = {buy[index][2]: index for index in range(len(buy))}
             else:
                 # Selling
                 #print "S"
-                heapq.heappush(sell, [float(each_values[4]), float(each_values[5]), each_values[2]])
+                heapq.heappush(sell, [float(each_values[4]), float(each_values[5]), each_values[2], each_values[0]])
                 sell_shares += int(each_values[5])
                 sell_dict = {sell[index][2]: index for index in range(len(sell))}
-                sell_shares_action(sell_shares, target_size, sell)
+                sell_set = sell_shares_action(sell_shares, target_size, sell, each_values[0])
                 sell_dict = {sell[index][2]: index for index in range(len(sell))}
         else:
             #print "Reduce the share from the book"
@@ -101,10 +109,11 @@ def main(target_size):
                     heapq.heapify(buy)
                     buy_dict = {buy[index][2]: index for index in range(len(buy))}
                 if buy_shares >= target_size:
-                    buy_shares_action(buy_shares, target_size, buy)
+                    buy_set = buy_shares_action(buy_shares, target_size, buy, each_values[0])
                     buy_dict = {buy[index][2]: index for index in range(len(buy))}
                 elif prev_share >= target_size:
-                    print "time S NA"
+                    output.write(each_values[0] + " S NA")
+                    output.write("\n")
                 #print buy_dict
             else:
                 index = sell_dict[each_values[2]]
@@ -112,7 +121,7 @@ def main(target_size):
                 prev_share = sell_shares
                 sell_shares -=int(each_values[3])
                 # reduce sell shares
-                if sell[sell_dict[each_values[2]]][1] <= 0:
+                if sell[index][1] <= 0:
                     # remove it
                     sell[index] = sell[-1]
                     sell.pop()
@@ -123,9 +132,11 @@ def main(target_size):
                 this case isn't handled
                 '''
                 if sell_shares >= target_size:
-                    sell_shares_action(sell_shares, target_size, sell)
+                    sell_set = sell_shares_action(sell_shares, target_size, sell, each_values[0])
                     sell_dict = {sell[index][2]: index for index in range(len(sell))}
                 elif prev_share >= target_size:
-                    print "time B NA"
+                    output.write(each_values[0] +" B NA")
+                    output.write("\n")
 if __name__ == "__main__":
-    main(10000)
+    target_size = int(raw_input("Enter the target size"))
+    main(target_size)
